a9a13f93e9e09a0c38ef1950eb7b016a6a995500
Mritunjay Kumar <101097898+Mritunjay882@users.noreply.github.com>
2023-04-29 09:27:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function getFormvalue() {
-    //Write your code here
+  // Get a reference to the form element
+  var form = document.querySelector("form");
 
+  // Add an event listener to handle form submission
+  form.addEventListener("submit", function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Get the values of the fname and lname input fields
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+
+    // Display the result using alert()
+    alert("First Name: " + fname + "\nLast Name: " + lname);
+  });
 }
+
